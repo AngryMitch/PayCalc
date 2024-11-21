90a508211100d586f76f99b54f5b26b8a32993bd
AngryMitch <mitchellroemling@icloud.com>
2024-11-21 22:57:41 +1000

    Simple Nav Bar and Button Setup

---
+++ pay-calc-frontend/src/components/Layout/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="nav-main">
+        <NavButton buttonTitle="Signup" />
+    </div>
+</template>
+
+<script>
+import NavButton from '../UI/NavButton.vue'
+
+export default {
+    name: 'NavMain',
+    components: {
+        NavButton
+    },
+    props: {
+        accountname: String
+    }
+}
+</script>
+
+<style scoped>
+.nav-main {
+    height: 100px;
+    display: inline-block;
+}
+</style>
---
+++ pay-calc-frontend/src/components/UI/NavButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <button>
+        <img src="../../assets/account-icon.svg" height="15"/>
+        <p>{{ buttonTitle }}</p>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'NavButton',
+    props: {
+        buttonTitle: String
+    }
+}
+</script>
+
+<style scoped>
+
+button {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background-color: #1f1f1f;
+    border-radius: 5px;
+    border-style: none;
+    color: white;
+    padding: 5px;
+}
+
+p {
+    margin: 5px;
+}
+
+img {
+    margin: 5px;
+    filter: invert(81%) sepia(95%) saturate(0%) hue-rotate(121deg) brightness(105%) contrast(105%);
+}
+
+</style>
